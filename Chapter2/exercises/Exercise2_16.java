ba33200fff497ddd16e8a53e08444659ba44bc61
mariemueller-codes <51761122+mariemueller-codes@users.noreply.github.com>
2019-07-29 14:52:27 -0500

    Create Exercise2_16.java

---
+++ Chapter2/exercises/Exercise2_16.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercise2_16 {
+	public static void main(String[] args) {
+		Scanner user = new Scanner(System.in);
+		
+		System.out.print("Enter first integer: ");
+		int num1 = user.nextInt();
+		System.out.print("Enter second integer: ");
+		int num2 = user.nextInt();
+		
+		if(num1 == num2) {
+			System.out.println("These numbers are equal.");
+		} else if(num1 > num2) {
+			System.out.println("First integer is greater than second integer");
+		} else if(num1 > num2) {
+			System.out.println("First integer is less than second integer");
+		} else {
+			System.out.println("Invalid input");
+		}
+	}
+}
